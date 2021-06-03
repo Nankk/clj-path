54720befb47848d87498e916ebe4dcef51e7a715
Nankk Home <toc32768@yahoo.co.jp>
2021-06-04 03:00:08 +0900

    :wrench: Fix project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject net.nankk/clj-path "0.1.0"
+(defproject org.clojars.nankk/clj-path "0.1.0"
   :description "Node-like path functions for clojure"
   :url "https://github.com/Nankk/clj-path"
   :license {:name "EPL-1.0"
@@ -6,5 +6,7 @@
             :url "https://choosealicense.com/licenses/epl-1.0"
             :year 2021
             :key "epl-1.0"}
+  :repositories {"clojars" {:url "https://clojars.org/repo"
+                            :sign-releases false}}
   :dependencies [[org.clojure/clojure "1.10.1"]]
   :repl-options {:init-ns clj-path.core})
