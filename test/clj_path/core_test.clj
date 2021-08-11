fd1bd609344615ebc855682839754588e0611d9d
Nankk Home <toc32768@yahoo.co.jp>
2021-08-11 17:26:16 +0900

    :rotating_light: Add tests for windows

--- test/clj_path/core_test.clj
+++ test/clj_path/core_test.clj
@@ -5,7 +5,6 @@
 (def windows? (re-find #"(?i)windows" (System/getProperty "os.name")))
 
 (when-not windows?
-
   (deftest absolute-test
     (is (absolute? "/usr/bin"))
     (is (not (absolute? "./usr/bin")))
@@ -36,3 +35,32 @@
     (is (= (normalize "/etc/X11/xinit/../xkb") "/etc/X11/xkb"))
     (is (= (normalize "/etc/X11/xinit/../..") "/etc"))))
 
+(when windows?
+  (deftest absolute-test
+    (is (absolute? "C:\\Users\\user1"))
+    (is (not (absolute? ".\\usr\\bin")))
+    (is (not (absolute? "usr\\bin"))))
+
+  (deftest relative-test
+    (is (= (relative "C:\\foo\\bar\\baz" "C:\\foo\\baz") "..\\..\\baz")))
+
+  (deftest dirname-test
+    (is (= (dirname "C:\\foo\\bar\\baz") "C:\\foo\\bar")))
+
+  (deftest extname-test
+    (is (= (extname "C:\\foo\\bar\\baz.txt") ".txt"))
+    (is (= (extname "C:\\foo\\bar\\baz.") "."))
+    (is (= (extname "C:\\foo\\bar\\baz") "")))
+
+  (deftest basename-test
+    (is (= (basename "/home/user/doc.txt") "doc.txt"))
+    (is (= (basename "/home/user/doc.txt" ".txt") "doc")))
+
+  (deftest join-test
+    (is (= (join "C:\\Users" "user1" ".emacs.d")  "C:\\Users\\user1\\.emacs.d"))
+    (is (= (join "C:\\Users" "user1" ".." ".emacs.d")  "C:\\Users\\user1\\..\\.emacs.d"))
+    (is (= (join "C:\\Users" "user1" ".." "..")  "C:\\Users\\user1\\..\\..")))
+
+  (deftest normalize-test
+    (is (= (normalize "C:\\Users\\user1\\..\\.emacs.d")  "C:\\Users\\.emacs.d"))
+    (is (= (normalize "C:\\Users\\user1\\..\\..")  "C:\\"))))
